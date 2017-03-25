8227d3b6abf98cce91470ac41b561ac7a8f29d40
chairus <chairus06@gmail.com>
2017-03-25 13:52:51 +1030

    Update web_app.js

--- web_app.js
+++ web_app.js
@@ -8,6 +8,7 @@ document.querySelector(".btn-add-entry").addEventListener("click",function() {
     
     // Display the headers and buttons of edit entry page
     document.getElementById("edit-entry-header").style.display = "block";
+    document.getElementById("edit-entry").style.display = "block";
     document.querySelector(".save-and-del-btn").style.display = "block";
 });
 
@@ -20,6 +21,9 @@ document.querySelector(".btn-save-entry").addEventListener("click", function() {
     document.querySelector(".btn-add-entry").style.display = "block";
     
     // Hide the headers and buttons of edit entry page
+    document.getElementById("entry-title").value = "";
+    document.getElementById("entry-content").value = "";
     document.getElementById("edit-entry-header").style.display = "none";
     document.querySelector(".save-and-del-btn").style.display = "none";
+    document.getElementById("edit-entry").style.display = "none";
 })
